1346ada4f169239709dba127060b732f468e4a67
Aakash-20 <skyakash480@gmail.com>
2023-11-02 18:59:17 +0530

    dev : on_boarding.py added the files

--- app/models.py
+++ app/models.py
@@ -1,5 +1,111 @@
-from sqlalchemy import Column, String, BIGINT, Date, JSON, ForeignKey, CheckConstraint, Boolean, Float, Integer
+from sqlalchemy import Column, String, BIGINT, Date, JSON, ForeignKey, Boolean, Float, Integer
 from sqlalchemy.orm import validates, relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
+
+
+class Companies(Base):
+    __tablename__ = "companies"
+
+    company_id = Column(String, nullable=True, primary_key=True, unique=True)
+    company_name = Column(String, nullable=True, unique=True)
+    company_password = Column(String, nullable=False)
+    company_domain = Column(String, nullable=True)
+    company_logo = Column(JSON, nullable=True)
+    company_email = Column(String, nullable=True)
+    services = Column(String, nullable=True)
+    company_contact = Column(BIGINT, nullable=True)
+    company_address = Column(String, nullable=True)
+    white_labelled = Column(Boolean, nullable=True)
+    onboarding_date = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
+
+    @validates('company_name', 'company_password', 'company_email', 'company_contact', 'company_address')
+    def empty_string_to_null(self, key, value):
+        if isinstance(value, str) and value == '':
+            return None
+        else:
+            return value
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    user_uniqueid = Column(BIGINT, primary_key=True, nullable=False,
+                           server_default=text("EXTRACT(EPOCH FROM NOW())::BIGINT"))
+    user_name = Column(String, nullable=True)
+    user_contact = Column(BIGINT, nullable=True)
+    user_birthdate = Column(Date, nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    user_image = Column(String, nullable=True)
+    user_emailId = Column(String, nullable=True)
+    user_password = Column(String, nullable=True)
+
+    @validates('user_contact', 'user_uniqueid')
+    def empty_string_to_null(self, key, value):
+        if isinstance(value, str) and value == '':
+            return None
+        else:
+            return value
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    category_id = Column(BIGINT, nullable=False, primary_key=True, autoincrement=True, unique=True)
+    category_name = Column(String, nullable=False)
+
+    @validates('category_name')
+    def empty_string_to_null(self, key, value):
+        if isinstance(value, str) and value == '':
+            return None
+        return value
+
+
+class Products(Base):
+    __tablename__ = "products"
+
+    product_id = Column(BIGINT, nullable=False, primary_key=True, autoincrement=True, unique=True, index=True)
+    brand_id = Column(BIGINT, ForeignKey("brands.brand_id", ondelete="CASCADE"), nullable=False)
+    product_name = Column(String, nullable=False)
+    category_id = Column(BIGINT, ForeignKey("categories.category_id", ondelete="CASCADE"), nullable=False)
+
+    category = relationship("Category")
+    brand = relationship("Brand")
+
+
+class ProductVariant(Base):
+    __tablename__ = "product_variants"
+
+    variant_id = Column(BIGINT, primary_key=True, index=True, autoincrement=True)
+    variant_cost = Column(Float, nullable=False)
+    brand_name = Column(String, nullable=False)
+    quantity = Column(Integer, nullable=False)
+    discounted_cost = Column(Float, nullable=True)
+    discount = Column(BIGINT, nullable=True)
+    stock = Column(Integer, nullable=False)
+    description = Column(String, nullable=False)
+    image = Column(JSON, nullable=True)
+    ratings = Column(Integer, nullable=True)
+    measuring_unit = Column(String, nullable=False)
+    is_published = Column(Boolean, nullable=False)
+    barcode_no = Column(BIGINT, nullable=False, unique=True)
+    product_id = Column(BIGINT, ForeignKey("products.product_id", ondelete="CASCADE"), nullable=False)
+    branch_id = Column(BIGINT, ForeignKey("branch.branch_id", ondelete="CASCADE"), nullable=False)
+
+    product = relationship("Products")
+    branch = relationship("Branch")
+
+
+class Branch(Base):
+    __tablename__ = "branch"
+
+    branch_id = Column(Integer, primary_key=True, autoincrement=True)
+    branch_name = Column(String, nullable=False)
+    branch_address = Column(String, nullable=False)
+    branch_email = Column(String, nullable=False)
+    branch_number = Column(BIGINT, nullable=False, unique=True)
+    company_id = Column(String, ForeignKey("companies.company_id", ondelete="CASCADE"))
+
+    company = relationship("Companies")
+
--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,59 @@
-from typing import Optional, List
+from typing import Optional
 from pydantic import BaseModel, EmailStr
-from datetime import date, datetime
+
+
+class Companies(BaseModel):
+    company_id: Optional[int] | None = None
+    company_name: str
+    company_password: str | None = None
+    company_domain: str
+    company_logo: str
+    company_email: EmailStr | None = None
+    services: str
+    company_contact: int
+    company_address: str
+    white_labelled: bool = True
+
+    class Config:
+        from_attributes = True
+
+
+class CompanyUpdateDetails(BaseModel):
+    company_name: str
+    company_domain: str
+    company_contact: int
+    company_address: str
+    white_labelled: bool = True
+
+    class Config:
+        from_attributes = True
+
+
+class CompanySignUp(BaseModel):
+    company_id: Optional[str] | None = None
+    company_email: str | None = None
+    company_contact: int | None = None
+    company_password: str
+
+    class Config:
+        from_attributes = True
+
+
+class LoginFlow(BaseModel):
+    company_contact: int | None = None
+    company_email: str | None = None
+    employee_contact: int | None = None
+    login_password: str
+
+    class Config:
+        from_attributes = True
+
+
+class NewUsers(BaseModel):
+    user_uniqueid: int | None = None
+    user_name: str | None = None
+    user_contact: str | None = None
+    user_birthdate: str | None = None
+    user_image: str | None = None
+    user_emailId: str | None = None
+    user_password: str
