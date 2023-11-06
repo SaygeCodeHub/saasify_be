393207a23b6915027d05a140a772061ff56c2e8f
Aditi-005 <aditi.diwan005@gmail.com>
2023-11-06 15:20:56 +0530

    SAS-00: Debug Authentication API error.

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://db_saasify_user:V45gnfQG3nZzPXzTRId6iE0hP3wL3X23@dpg-cl3idg0t3kic73dclg0g-a.oregon-postgres.render.com/db_saasify"
+SQLALCHEMY_DATABASE_URL = "postgresql://db_saasify_user:V45gnfQG3nZzPXzTRId6iE0hP3wL3X23@dpg-cl3idg0t3kic73dclg0g-a.oregon-postgres.render.com/db_saasify?sslmode=require"
 
 # "postgresql://postgres:Mun1chad$@localhost/postgres"
 #
