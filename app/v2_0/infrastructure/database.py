44cbf1339c676208066bd801841267d188dc333c
Aditi-005 <111351516+Aditi-005@users.noreply.github.com>
2024-01-20 14:21:22 +0530

    Update database url.

--- app/v2_0/infrastructure/database.py
+++ app/v2_0/infrastructure/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgres://default:hurR7WSdp6wl@ep-wild-term-46659373.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+SQLALCHEMY_DATABASE_URL = "postgresql://default:hurR7WSdp6wl@ep-wild-term-46659373.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/postgres"  # Jayraj's local instance for v1.1
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/saasify_2.0"  # Jayraj's local instance for v2.0
