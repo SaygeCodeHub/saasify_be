1f62cefddb154c91a0f4b4f1f10ef277d5140f9d
Jayraj Manoj Deshmukh <jayraj.manoj@gmail.com>
2024-01-31 18:01:44 +0530

    SAAS-37: 1.Added the get APIs to Designations and Department flow

--- app/v2_0/application/service/module_service.py
+++ app/v2_0/application/service/module_service.py
@@ -1,8 +1,10 @@
 """Service layer for Modules"""
 from app.v2_0.application.dto.dto_classes import ResponseDTO
 from app.v2_0.application.utility.app_utility import check_if_company_and_branch_exist, add_module_to_ucb
+from app.v2_0.domain.models.enums import Modules
 from app.v2_0.domain.models.module_subscriptions import ModuleSubscriptions
 from app.v2_0.domain.models.user_auth import UsersAuth
+from app.v2_0.domain.schemas.module_schemas import GetSubscribedModules, ModuleInfoResponse
 
 
 def modify_company_modules():
@@ -10,6 +12,7 @@ def modify_company_modules():
 
 
 def add_module(module, user_id, branch_id, company_id, db):
+    """Adds modules to a company"""
     try:
         user = db.query(UsersAuth).filter(UsersAuth.user_id == user_id).first()
 
@@ -27,6 +30,60 @@ def add_module(module, user_id, branch_id, company_id, db):
             add_module_to_ucb(branch_id, user_id, module.modules, db)
 
             return ResponseDTO(200, "Module(s) added successfully", {})
+        else:
+            return check
+    except Exception as exc:
+        return ResponseDTO(204, str(exc), {})
+
+
+def fetch_subscribed_modules(user_id, company_id, branch_id, db):
+    try:
+        user = db.query(UsersAuth).filter(UsersAuth.user_id == user_id).first()
+
+        if user is None:
+            return ResponseDTO(404, f"User with user id: {user_id} not found", {})
+
+        check = check_if_company_and_branch_exist(company_id, branch_id, db)
+
+        if check is None:
+            modules = db.query(ModuleSubscriptions).filter(ModuleSubscriptions.branch_id == branch_id).all()
+
+            if len(modules) == 0:
+                return ResponseDTO(200, "No modules to fetch!", {})
+
+            result = [GetSubscribedModules(module_name=module.module_name.name, is_subscribed=module.is_subscribed,
+                                           start_date=module.start_date, end_date=module.end_date)
+                      for module in modules
+                      ]
+
+            return ResponseDTO(200, "Modules fetched!", result)
+
+        else:
+            return check
+    except Exception as exc:
+        return ResponseDTO(204, str(exc), {})
+
+
+def fetch_all_modules(user_id, company_id, branch_id, db):
+    try:
+        user = db.query(UsersAuth).filter(UsersAuth.user_id == user_id).first()
+
+        if user is None:
+            return ResponseDTO(404, f"User with user id: {user_id} not found", {})
+
+        check = check_if_company_and_branch_exist(company_id, branch_id, db)
+
+        if check is None:
+
+            all_modules = list(Modules.__members__)
+
+            result = [
+                ModuleInfoResponse(module_name=module)
+                for module in all_modules
+            ]
+
+            return ResponseDTO(200, "Modules fetched!", result)
+
         else:
             return check
     except Exception as exc:
