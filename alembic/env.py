5be5197ef46dbca0c1eacaf56a48a55d8ce14787
Aditi-005 <aditi.diwan005@gmail.com>
2024-02-03 18:59:25 +0530

    SAAS-00: Fix Get User by id API.

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,7 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from app.v2_0.domain.models.import_models import Base
+from app.v2_0.infrastructure.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
