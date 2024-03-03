274b83801768b65dda873f2307d8c6b14a345bae
Jayraj Manoj Deshmukh <jayraj.manoj@gmail.com>
2024-03-03 15:43:31 +0530

    SAAS-00: Implemented nested for loops for extracting values from post body of announcement dynamic forms

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.infrastructure.db_config import DATABASE_URL_LOCAL_JAYRAJ
+from app.infrastructure.db_config import DATABASE_URL_PROD
 
-SQLALCHEMY_DATABASE_URL = DATABASE_URL_LOCAL_JAYRAJ
+SQLALCHEMY_DATABASE_URL = DATABASE_URL_PROD
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_recycle=300)
 
